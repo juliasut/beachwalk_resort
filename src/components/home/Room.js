18c070fa5bb8f3ed901a6eaf52f660fe1d4ec1fd
juliasut <juliasutygina@gmail.com>
2021-08-15 15:45:22 -0600

    Add jsx and propTypes check

--- src/components/home/Room.js
+++ src/components/home/Room.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { SmallBtn } from '../globals/Buttons'
+import { SmallBtn } from '../globals/Buttons';
+import styled from 'styled-components';
+import {
+  setRem,
+  setLetterSpacing,
+  setTransition,
+  setShadow,
+  setBorder,
+  setColor,
+} from '../../styles';
+import PropTypes from 'prop-types';
 
-const Room = () => {
+const Room = ({ className, room }) => {
+  const { img = '', title = '', info = '', price = 0 } = room;
   return (
-    <div>
-      <SmallBtn>SmallBtn</SmallBtn>
-    </div>
-  )
-}
+    <article className={className}>
+      <div className="img-container">
+        <img src={img} alt="sigle room" />
+        <div className="price">${price}</div>
+      </div>
+      <div className="room">
+        <h4>{title}</h4>
+        <p>{info}</p>
+        <SmallBtn>SmallBtn</SmallBtn>
+      </div>
+    </article>
+  );
+};
 
-export default Room;
+export default styled(Room)``;
+
+Room.propTypes = {
+  room:PropTypes.shape({
+    img:PropTypes.string.isRequired,
+    title:PropTypes.string.isRequired,
+    info:PropTypes.string.isRequired,
+    price:PropTypes.number.isRequired,
+  })
+}
