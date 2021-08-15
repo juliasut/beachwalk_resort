51e45994d480b1ee6b04ca931eed84b55e9a6aeb
juliasut <juliasutygina@gmail.com>
2021-08-15 16:09:19 -0600

    Add css to single room component

--- src/components/home/Room.js
+++ src/components/home/Room.js
@@ -19,7 +19,7 @@ const Room = ({ className, room }) => {
         <img src={img} alt="sigle room" />
         <div className="price">${price}</div>
       </div>
-      <div className="room">
+      <div className="room-info">
         <h4>{title}</h4>
         <p>{info}</p>
         <SmallBtn>SmallBtn</SmallBtn>
@@ -28,13 +28,60 @@ const Room = ({ className, room }) => {
   );
 };
 
-export default styled(Room)``;
+export default styled(Room)`
+  background: ${setColor.mainWhite};
+  margin: ${setRem(32)} 0;
+  .img-container {
+    background: ${setColor.mainBlack};
+    position: relative;
+    img {
+      width: 100%;
+      display: block;
+      ${setTransition};
+    }
+    &:hover img {
+      opacity: 0.5;
+    }
+    .price {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: ${setColor.mainWhite};
+      ${setLetterSpacing(5)};
+      font-size: ${setRem(22)};
+      padding: ${setRem(10)} ${setRem(33)};
+      ${setBorder({ color: setColor.mainWhite })};
+      opacity: 0;
+      ${setTransition()};
+    }
+    &:hover .price {
+      opacity: 1;
+    }
+  }
+  .room-info {
+    padding: ${setRem()};
+    h4 {
+      text-transform: capitalize;
+      ${setLetterSpacing()};
+    }
+    p {
+      ${setLetterSpacing};
+    }
+  }
+
+  ${setShadow.light};
+  ${setTransition};
+  &:hover {
+    ${setShadow.dark};
+  }
+`;
 
 Room.propTypes = {
-  room:PropTypes.shape({
-    img:PropTypes.string.isRequired,
-    title:PropTypes.string.isRequired,
-    info:PropTypes.string.isRequired,
-    price:PropTypes.number.isRequired,
-  })
-}
+  room: PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    info: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }),
+};
