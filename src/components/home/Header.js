c1bb81711b34164e62844c628e7cdc7ef23873f9
juliasut <juliasutygina@gmail.com>
2021-08-15 02:06:52 -0600

    Add Banner properties and media queries

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Hero from '../globals/Hero';
 import aboutImg from '../../images/aboutBcg.jpeg';
 import homeImg from '../../images/homeBcg.jpeg';
+import Banner from '../globals/Banner';
 
 export const Header = () => {
   return (
     <Hero img={homeImg}>
-      <h1>Hello</h1>
+      <Banner greeting="welcome to" title="beachwalk resort" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ad voluptatem suscipit obcaecati eaque, reiciendis accusantium? Mollitia, assumenda tempora! Nulla porro minima quo delectus voluptates!"  />
+      
     </Hero>
-  )
-}
+  );
+};
